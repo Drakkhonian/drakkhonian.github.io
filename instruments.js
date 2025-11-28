7b2c60110abfda34bb771005109b2a323e139d42
Felipe Vieira <felipe.a.p.vieira@hotmail.com>
2025-11-28 19:40:44 -0300

    element check 3

--- instruments.js
+++ instruments.js
@@ -34,7 +34,7 @@ const attack_shape_map = [[10, 11, 10, 10, 6, 12, 13, 12],
 
 
 function generate_button_clicked(){
-    document.getElementById("results").innerHTML = "results generated";
+    document.getElementById("results").innerHTML = "element check 3";
     for (let element_index = 0; element_index <element_list.length; element_index++){
         const current_element = element_list[element_index];
 
