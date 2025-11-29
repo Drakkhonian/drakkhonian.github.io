ac4c9bc5a8b9cb762b31a997171602702329db52
Felipe Vieira <felipe.a.p.vieira@hotmail.com>
2025-11-28 21:10:31 -0300

    migrate to strucuters 1

--- instruments.js
+++ instruments.js
@@ -1,43 +1,6 @@
-import { primaries, primaries_names, coin_list } from "./structures.js";
+import { primaries, primaries_names, coin_list, element_list, area_type, target_type, instrument_type, attack_shape_map } from "./structures.js";
 
-const element_list =["Wisp",
-    "Shade",
-    "Dryad",
-    "Aura",
-    "Salamander",
-    "Gnome",
-    "Jinn",
-    "Undine",
-];
 
-const area_type =["Circle",
-    "Donut",
-    "Fan",
-    "Line",
-    "Random",
-    "Sphere",
-    "Control"
-];
-
-const target_type =["Fire",
-    "Lock"
-];
-
-const instrument_type =[
-    "Harp",
-    "Marimba",
-    "Flute",
-    "Drum"
-]
-
-const attack_shape_map = [[10, 11, 10, 10, 6, 12, 13, 12],
-[11, 11, 10, 7, 6, 7, 13, 13],
-[10, 10, 4, 4, 7, 12, 6, 12],
-[10, 4, 10, 6, 4, 6, 8, 12],
-[0, 4, 5, 5, 4, 9, 6, 12],
-[0, 4, 0, 8, 4, 2, 8, 8],
-[1, 1, 4, 4, 9, 8, 3, 3],
-[0, 1, 0, 0, 2, 2, 3, 2]];
 
 
 function generate_button_clicked(){
--- structures.js
+++ structures.js
@@ -293,3 +293,42 @@ export const secondaries_names = [
     "pouches",
     "secondary misc"
 ];
+
+export const element_list =["Wisp",
+    "Shade",
+    "Dryad",
+    "Aura",
+    "Salamander",
+    "Gnome",
+    "Jinn",
+    "Undine",
+];
+
+export const area_type =["Circle",
+    "Donut",
+    "Fan",
+    "Line",
+    "Random",
+    "Sphere",
+    "Control"
+];
+
+export const target_type =["Fire",
+    "Lock"
+];
+
+export const instrument_type =[
+    "Harp",
+    "Marimba",
+    "Flute",
+    "Drum"
+]
+
+const attack_shape_map = [[10, 11, 10, 10, 6, 12, 13, 12],
+[11, 11, 10, 7, 6, 7, 13, 13],
+[10, 10, 4, 4, 7, 12, 6, 12],
+[10, 4, 10, 6, 4, 6, 8, 12],
+[0, 4, 5, 5, 4, 9, 6, 12],
+[0, 4, 0, 8, 4, 2, 8, 8],
+[1, 1, 4, 4, 9, 8, 3, 3],
+[0, 1, 0, 0, 2, 2, 3, 2]];
